ef1e148f44858f18c479903ec16b7fcc129b8d35
Jatin Anand <jatinanand2087@gmail.com>
2023-02-21 00:16:20 +0530

    Delete 1929. Concatenation of Array.py

--- 1929. Concatenation of Array.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def getConcatenation(self, nums: List[int]) -> List[int]:
-        ans=[]
-        for i in range(len(nums)):
-            ans.append(nums[i])
-        for j in range(len(nums)):
-            ans.append(nums[j])
-        return ans
